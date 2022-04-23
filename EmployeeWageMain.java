47eeec2e9938c3f177a6a86f9b596f655dfa0174
Padmajase <padmajapawar7@gmail.com>
2022-04-22 22:11:01 -0700

    Added : Employee wage UC1 from Day10

--- EmployeeWageMain.java
+++ EmployeeWageMain.java
@@ -1,3 +1,23 @@
+import java.util.Random;
+
+class EmployeeWageMain {
+	public static void main(String args[]) {
+	System.out.println("Welcome To Employee Wage Computation Program !");
+	
+	/** Checking Employee is Present or Absent**/ 
+
+	Random rand = new Random();
+	int isEmployee = rand.nextInt(2);	
+	System.out.println(isEmployee);
+
+	if(isEmployee == 1){
+		System.out.println("Employee is Present");
+	} else {
+		System.out.println("Employee is Absent");
+	}
+
+	}
+}
 class EmployeeWage {
 
 	public static void main(String args[]) {
